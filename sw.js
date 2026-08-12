08a4de28e55aa65372e80db48fc3be310bb42847
rociomaica-crypto <rociomaica@gmail.com>
2026-08-12 11:02:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'mv-calidad-v2';
-const SHELL = ['./manifest.json', './icon-192.png', './icon-512.png'];
+const SHELL = ['./manifest.json', './icon-192.png', './icon-512.png', './logo-mv.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
